318e1501b2dac8a60421038426e40081bae0c340
plac <asdf@asdf.xor>
2014-06-18 01:49:58 +0200

    another higher version try

--- smz/test.user.js
+++ smz/test.user.js
@@ -5,5 +5,5 @@
 // @include        http://www.surrealmoviez.info/
 // @updateURL      https://raw.githubusercontent.com/surrealcode/smz-userscripts/master/smz/test.user.js
 // @downloadURL    https://raw.githubusercontent.com/surrealcode/smz-userscripts/master/smz/test.user.js
-// @version        2
+// @version        2.0
 // ==/UserScript==
