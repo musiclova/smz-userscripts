466e0b9810a2cb3e2df0dc85e1ce51c1b9e5dfee
plac <asdf@asdf.xor>
2014-09-02 01:25:44 +0200

    script tested

---
+++ imdb/imdb.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           IMDb Enhancer
+// @namespace      surrealmoviez.info
+// @description    Better display for IMDb
+// @updateURL      
+// @downloadURL    
+// @include        http://www.imdb.com/find?q=*
+// @grant          none
+// @version        0.0.1
+// ==/UserScript==
+
+// Insert the IMDb ID as plain text at the right side of the titles matches
+var titlesBlock = $('.findSectionHeader:contains("Titles")').parent();
+$('.result_text', titlesBlock).each(function() {
+    var href = $('a', this).attr('href');
+    var id = href.substring(href.indexOf('/tt') + 1, href.lastIndexOf('/'));
+    $(this).append('<span style="float: right; margin-right: 5px; opacity: 0.4; padding: 4px;">' + id + '</span>');
+});
