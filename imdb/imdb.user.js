0c13bf88da5d1bdcf291d8a8dfbf4be45ea643a2
plac <asdf@asdf.xor>
2014-09-03 01:27:29 +0200

    @include updated

--- imdb/imdb.user.js
+++ imdb/imdb.user.js
@@ -4,9 +4,9 @@
 // @description    Better display for IMDb
 // @updateURL      https://github.com/surrealcode/smz-userscripts/raw/master/imdb/imdb.user.js
 // @downloadURL    https://github.com/surrealcode/smz-userscripts/raw/master/imdb/imdb.user.js
-// @include        http://www.imdb.com/find?q=*
+// @include        http://www.imdb.com/find?*
 // @grant          none
-// @version        0.0.1
+// @version        0.0.2
 // ==/UserScript==
 
 // Insert the IMDb ID as plain text at the right side of the titles matches
